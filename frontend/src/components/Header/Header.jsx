3b5897b12904bd151078f50e2f85a7629c326e9b
Pratibha Arya <pratibhakag.s29@gmail.com>
2026-06-05 13:27:36 +0530

    ramji-updated

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,9 +5,7 @@ const Header = () => {
     <div className="header">
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
-        <p>
-          Choose from a diverse menu featuring a delectable array of dishes.
-        </p>
+        <p>Ramji restauratn </p>
         <button>View Menu</button>
       </div>
     </div>
