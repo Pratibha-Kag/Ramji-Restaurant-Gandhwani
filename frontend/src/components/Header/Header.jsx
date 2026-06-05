847a3a01f3265a34380280484826876ade35d9e0
Pratibha Arya <pratibhakag.s29@gmail.com>
2026-06-05 12:24:39 +0530

    header-section

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,10 +6,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
         <p>
-          Choose from a diverse menu featuring a delectable array of dishes
-          crafted with the finest ingredient and culinary to satisfy you and
-          your craving and elevate your dinning experience , one delicoius meal
-          at a time.
+          Choose from a diverse menu featuring a delectable array of dishes.
         </p>
         <button>View Menu</button>
       </div>
