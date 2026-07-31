67d53ffaf9364a114b0a5a2bdd0aadea636735ec
Pratibha Arya <pratibhakag.s29@gmail.com>
2026-07-31 22:11:17 +0530

    ios

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -9,7 +9,13 @@ const paymentRouter = express.Router();
 console.log("paymentRoute.js loaded");
 paymentRouter.post("/create-order", authMiddleware, createOrder);
 
-paymentRouter.post("/verify-payment", authMiddleware, verifyPayment);
+paymentRouter.post("/verify-payment", (req, res) => {
+  console.log("VERIFY ROUTE HIT");
+  res.json({
+    success: true,
+    message: "Route Working",
+  });
+});
 paymentRouter.get("/test", (req, res) => {
   res.send("Payment Route Working");
 });
