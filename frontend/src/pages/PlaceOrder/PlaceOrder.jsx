923c7403aa875ff49d05e18d99e0ed28f2751164
Pratibha Arya <pratibhakag.s29@gmail.com>
2026-07-31 16:01:23 +0530

    pop

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -73,7 +73,9 @@ const PlaceOrder = () => {
           color: "#C97B36",
         },
       };
-
+      console.log("Razorpay Key:", import.meta.env.VITE_RAZORPAY_KEY_ID);
+      console.log("Options:", options);
+      console.log("Razorpay:", window.Razorpay);
       const razorpay = new window.Razorpay(options);
 
       razorpay.open();
